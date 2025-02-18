7264e508f25c0c0f8f2eafb206f8421f7301c716
Michal Gawik <gawlik.zator@o2.pl>
2025-02-18 08:09:02 +0100

    create exports

---
+++ src/pages/index.ts
@@ -0,0 +1,25 @@
+import AboutPage from './AboutPage'
+import CartPage from './CartPage'
+import CheckoutPage from './CheckoutPage'
+import ErrorPage from './ErrorPage'
+import HomeLayoutPage from './HomeLayoutPage'
+import LandingPage from './LandingPage'
+import LoginPage from './LoginPage'
+import OrdersPage from './OrdersPage'
+import ProductsPage from './ProductsPage'
+import RegisterPage from './RegisterPage'
+import SingleProductPage from './SingleProductPage'
+
+export {
+	AboutPage,
+	CartPage,
+	CheckoutPage,
+	ErrorPage,
+	HomeLayoutPage,
+	LandingPage,
+	LoginPage,
+	OrdersPage,
+	ProductsPage,
+	RegisterPage,
+	SingleProductPage,
+}
