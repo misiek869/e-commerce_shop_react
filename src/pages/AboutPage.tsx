4c388e11ec903db288d286bec1c41395a08bf842
Michal Gawik <gawlik.zator@o2.pl>
2025-02-18 14:36:54 +0100

    create page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
 const AboutPage = () => {
-	return <div>AboutPage</div>
+	return (
+		<section>
+			<h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>
+				A new era of
+				<span className='bg-yellow-400 py-4  px-6 rounded-sm tracking-widest text-white'>
+					shopping
+				</span>
+			</h1>
+			<p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground'>
+				Welcome to a revolutionary shopping experience designed with you in
+				mind. Say goodbye to long lines and complicated processes. With our
+				user-friendly platform, you can explore a curated selection of products
+				at your fingertips. Enjoy seamless browsing, personalized
+				recommendations, and hassle-free checkout. Join us as we redefine
+				convenience and make shopping a pleasure, not a chore. Experience the
+				future of shopping today!
+			</p>
+		</section>
+	)
 }
 
 export default AboutPage
