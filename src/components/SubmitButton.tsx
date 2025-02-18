31a965632b48a90e30caf92bd3e08fa4fec24ae4
Michal Gawik <gawlik.zator@o2.pl>
2025-02-18 11:32:08 +0100

    add isSubmitting

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useNavigation } from 'react-router-dom'
 
 const SubmitButton = ({ text }: { text: string }) => {
-	return <button className='btn btn-primary'>{text}</button>
+	const navigation = useNavigation()
+
+	const isSubmitting = navigation.state === 'submitting'
+
+	return (
+		<button className='btn btn-primary' disabled={isSubmitting}>
+			{isSubmitting ? (
+				<>
+					<span className='loading loading-ring loading-md'></span>
+				</>
+			) : (
+				text
+			)}
+		</button>
+	)
 }
 
 export default SubmitButton
