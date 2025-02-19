65cce20c69b2a736506add4c8643500da4dd36c6
Michal Gawik <gawlik.zator@o2.pl>
2025-02-19 11:52:25 +0100

    create formatPrice function

---
+++ src/utils/formatPrice.ts
@@ -0,0 +1,10 @@
+export const formatPrice = (amount: number): number => {
+	const dollarsPrice = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+	})
+		.format(amount / 100)
+		.toFixed(2)
+
+	return dollarsPrice
+}
