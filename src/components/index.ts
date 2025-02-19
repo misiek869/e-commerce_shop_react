6d057e3d02b8f2f705d1d70cfadb143e73d17cef
Michal Gawik <gawlik.zator@o2.pl>
2025-02-19 10:42:48 +0100

    export more components

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,5 +5,19 @@ import Navbar from './Navbar'
 import NavLinks from './NavLinks'
 import Hero from './Hero'
 import ErrorElement from './ErrorElement'
+import SectionTitle from './SectionTitle'
+import ProductsGrid from './ProductsGrid'
+import FeaturedProducts from './FeaturedProducts'
 
-export { FormInput, SubmitButton, Header, Navbar, NavLinks, Hero, ErrorElement }
+export {
+	FormInput,
+	SubmitButton,
+	Header,
+	Navbar,
+	NavLinks,
+	Hero,
+	ErrorElement,
+	SectionTitle,
+	ProductsGrid,
+	FeaturedProducts,
+}
