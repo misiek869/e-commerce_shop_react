8296494e98f0f788a6360748edf8c0325cb4c752
Michal Gawik <gawlik.zator@o2.pl>
2025-03-05 13:13:38 +0100

    create component body

--- src/components/CartTotals.tsx
+++ src/components/CartTotals.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import { formatPrice } from '../utils/formatPrice'
 
 const CartTotals = () => {
-	return <div>CartTotals</div>
+	const { cartTotal, shipping, tax, orderTotal } = useSelector(
+		state => state.cartState
+	)
+
+	return (
+		<div className='card bg-base-200'>
+			<div className='card-body'>
+				<p className='flex justify-between text-xs border-b border-b-base-300 pb-2'>
+					<span>Subtotal</span>
+					<span>{formatPrice(cartTotal)}</span>
+				</p>
+				<p className='flex justify-between text-xs border-b border-b-base-300 pb-2 border-b-'>
+					<span>Shipping</span>
+					<span>{formatPrice(shipping)}</span>
+				</p>
+				<p className='flex justify-between text-xs border-b border-b-base-300 pb-2 border-b-'>
+					<span>Tax</span>
+					<span>{formatPrice(tax)}</span>
+				</p>
+				<p className='mt-4 flex justify-between text-sm pb-2'>
+					<span className='font-bold'>Total</span>
+					<span className='font-bold'>{formatPrice(orderTotal)}</span>
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default CartTotals
